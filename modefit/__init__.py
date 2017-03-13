9985138e7b7bae435a85f8b14444a0f17ed63e19
Mickael <mick.rigault@gmail.com>
2017-03-13 10:36:07 +0100

    Cleaning and setup

--- modefit/__init__.py
+++ modefit/__init__.py
@@ -1,10 +1,7 @@
 """ Classes and Functions allowing advance fitting techniques. """
 
+__version__ = 0.1.0
+
 from unimodal import *
 from bimodal  import *
-from hubble   import get_hubblefit
-from basics import get_polyfit,get_normpolyfit
-try:
-    from emissionlines import linefit
-except:
-    warning.warns("Cannot import emissionlines. Most likely you do not have astrobject.")
+from basics import get_polyfit, get_normpolyfit
---
+++ setup.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+#
+
+DESCRIPTION = "modefit: Simple minuit<->mcmc fitting tools."
+LONG_DESCRIPTION = """ Simple minuit<->mcmc fitting tools. """
+
+DISTNAME = 'modefit'
+AUTHOR = 'Mickael Rigault'
+MAINTAINER = 'Mickael Rigault' 
+MAINTAINER_EMAIL = 'mrigault@physik.hu-berlin.de'
+URL = 'https://github.com/MickaelRigault/modefit/'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/modefit/tarball/0.1'
+VERSION = '0.1.0'
+
+try:
+    from setuptools import setup, find_packages
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+def check_dependencies():
+   install_requires = []
+
+   # Just make sure dependencies exist, I haven't rigorously
+   # tested what the minimal versions that will work are
+   # (help on that would be awesome)
+   try:
+       import propobject
+   except ImportError:
+       install_requires.append('propobject')
+
+    return install_requires
+
+if __name__ == "__main__":
+
+    install_requires = check_dependencies()
+
+    if _has_setuptools:
+        packages = find_packages()
+        print packages
+    else:
+        # This should be updated if new submodules are added
+        packages = ['modefit']
+
+    setup(name=DISTNAME,
+          author=AUTHOR,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=install_requires,
+          packages=packages,
+          package_data={'modefit': []},
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Programming Language :: Python :: 2.7',
+              'License :: OSI Approved :: BSD License',
+              'Topic :: Scientific/Engineering :: Astronomy',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS'],
+      )
